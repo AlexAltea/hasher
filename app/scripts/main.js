365c31e4b769a0d191eb7bd10083c6f054fb7ace
Alexandro Sánchez Bach <alexandro.sanchez@alumni.uni-heidelberg.de>
2016-06-18 01:46:56 +0200

    Selector for text/hex/base64 format on inputs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,12 +17,68 @@ clipboard.on('success', function(e) {
 // AngularJS
 var H = angular.module('Hasher', ['ui.bootstrap']);
 
+// Input
+function HashInput (mode, value) {
+    this.mode = mode;
+    this.value = value;
+
+    this.get = function () {
+        if (this.valid()) {
+            // Plain-text
+            if (this.mode == 'A') {
+                return this.value;
+            }
+            // Base 64
+            if (this.mode == 'B') {
+                return atob(this.value);
+            }
+            // Hexadecimal
+            if (this.mode == 'X') {
+                var str = '', hex = this.value.replace(' ', '');
+                for (var i = 0; i < hex.length; i += 2)
+	                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+                return str;
+            }
+        } else {
+            return '';
+	    }
+    }
+    this.valid = function () {
+        switch (this.mode) {
+        case 'A': // Plain-text
+            return true;
+        case 'B': // Base 64
+            return /^([0-9A-Za-z+/]{4})*(([0-9A-Za-z+/]{2}==)|([0-9A-Za-z+/]{3}=))?$/.test(this.value);
+        case 'X': // Hexadecimal
+            return /^[0-9A-Fa-f ]*$/.test(this.value);
+        default:
+            return false;
+        }
+    }
+    this.convert = function (mode) {
+        this.value = this.valid() ? this.get() : this.value;
+        this.mode = mode;
+        // Base 64
+        if (this.mode == 'B') {
+            this.value = btoa(this.value);
+        }
+        // Hexadecimal
+        if (this.mode == 'X') {
+            var hex = '', str = this.value;
+	        for (var i = 0; i < str.length; i++)
+		        hex += ("00" + str.charCodeAt(i).toString(16)).slice(-2);
+	        this.value = hex;
+        }
+    } 
+}
+
+// Controllers
 H.controller('HasherController', ['$scope', function ($scope) {
     // Default
-    $scope.pass = { value: "123456",  mode: "A" };
-    $scope.salt = { value: "",        mode: "A" };
-    $scope.user = { value: "",        mode: "A" };
-    $scope.hash = { value: "",        mode: "X" };
+    $scope.pass = new HashInput("A", "123456");
+    $scope.salt = new HashInput("A", "");
+    $scope.user = new HashInput("A", "");
+    $scope.hash = new HashInput("X", "");
 
     // Methods
 	$scope.methods = [{
@@ -87,9 +143,9 @@ H.directive('hashOutput', function () {
 H.filter('filterMethods', function () {
     return function (methods, scope) {
         return methods.filter(function (method) {
-            var tPass = scope.pass.value;
-            var tSalt = scope.salt.value;
-            var tUser = scope.user.value;
+            var tPass = scope.pass.get();
+            var tSalt = scope.salt.get();
+            var tUser = scope.user.get();
 
             // Filter by used parameters
             switch (method.func.length) {
@@ -100,8 +156,8 @@ H.filter('filterMethods', function () {
                 return false;
             }
 
-            // Filter by expected hash
-            var expectedHash = scope.hash.value;
+            // Filter by expected hash (TODO: Only works if hash.mode is hexadecimal)
+            var expectedHash = scope.hash.get();
             var resultingHash = method.func(tPass, tUser, tUser);
             return (resultingHash.indexOf(expectedHash) > -1);
         });
