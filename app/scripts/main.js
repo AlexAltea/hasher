bfef5d3b8b4afed01a9d89f7ae90d403ff487617
Alexandro Sánchez Bach <alexandro.sanchez@alumni.uni-heidelberg.de>
2016-06-18 02:36:15 +0200

    Refactored code and added more methods

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,4 @@
+var md4 = require('js-md4');
 var md5 = require('md5');
 var sha1 = require('sha1');
 var sha256 = require('sha256');
@@ -14,8 +15,20 @@ clipboard.on('success', function(e) {
     }
 });
 
-// AngularJS
-var H = angular.module('Hasher', ['ui.bootstrap']);
+// Utils
+function stringToHex(str) {
+    var hex = '';
+	for (var i = 0; i < str.length; i++)
+	    hex += ("00" + str.charCodeAt(i).toString(16)).slice(-2);
+    return hex;
+}
+function hexToString(hex) {
+    var str = '';
+    hex = hex.replace(' ', '');
+	for (var i = 0; i < hex.length; i += 2)
+	    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    return str;
+}
 
 // Input
 function HashInput (mode, value) {
@@ -24,21 +37,9 @@ function HashInput (mode, value) {
 
     this.get = function () {
         if (this.valid()) {
-            // Plain-text
-            if (this.mode == 'A') {
-                return this.value;
-            }
-            // Base 64
-            if (this.mode == 'B') {
-                return atob(this.value);
-            }
-            // Hexadecimal
-            if (this.mode == 'X') {
-                var str = '', hex = this.value.replace(' ', '');
-                for (var i = 0; i < hex.length; i += 2)
-	                str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-                return str;
-            }
+            if (this.mode == 'A') { return this.value; }
+            if (this.mode == 'B') { return atob(this.value); }
+            if (this.mode == 'X') { return hexToString(this.value); }
         } else {
             return '';
 	    }
@@ -58,19 +59,13 @@ function HashInput (mode, value) {
     this.convert = function (mode) {
         this.value = this.valid() ? this.get() : this.value;
         this.mode = mode;
-        // Base 64
-        if (this.mode == 'B') {
-            this.value = btoa(this.value);
-        }
-        // Hexadecimal
-        if (this.mode == 'X') {
-            var hex = '', str = this.value;
-	        for (var i = 0; i < str.length; i++)
-		        hex += ("00" + str.charCodeAt(i).toString(16)).slice(-2);
-	        this.value = hex;
-        }
+        if (this.mode == 'B') { this.value = btoa(this.value); }
+        if (this.mode == 'X') { this.value = stringToHex(this.value); }
     } 
 }
+
+// AngularJS
+var H = angular.module('Hasher', ['ui.bootstrap']);
 
 // Controllers
 H.controller('HasherController', ['$scope', function ($scope) {
@@ -83,36 +78,101 @@ H.controller('HasherController', ['$scope', function ($scope) {
     // Methods
 	$scope.methods = [{
         name: 'MD5($pass)',
-        func: function (n1) { return md5(n1); },
+        func: function (p) { return md5(p); },
         hashcat: 0
     }, {
         name: 'MD5($pass.$salt)',
-        func: function (n1, n2) { return md5(n1 + n2); },
+        func: function (p, s) { return md5(p + s); },
         hashcat: 10
     }, {
-        name: 'MD5(MD5(MD5($pass)))',
-        func: function (n1) { return md5(md5(md5(n1))); },
-        hashcat: 3500
-    }, {
-        name: 'MD5($pass.MD5($salt))',
-        func: function (n1, n2) { return md5(n1 + md5(n2)); },
-        hashcat: 3720
-    }, {
-        name: 'MD5($salt.$pass.$salt)',
-        func: function (n1, n2) { return md5(n2 + n1 + n2); },
-        hashcat: 3800
+        name: 'MD5($salt.$pass)',
+        func: function (p, s) { return md5(s + p); },
+        hashcat: 20
     }, {
         name: 'SHA1($pass)',
-        func: function (n1) { return sha1(n1); },
+        func: function (p) { return sha1(p); },
         hashcat: 100
     }, {
         name: 'SHA1($pass.$salt)',
-        func: function (n1, n2) { return sha1(n1 + n2); },
+        func: function (p, s) { return sha1(p + s); },
         hashcat: 110
     }, {
+        name: 'SHA1($salt.$pass)',
+        func: function (p, s) { return sha1(s + p); },
+        hashcat: 120
+    }, {
+        name: 'MD4($pass)',
+        func: function (p) { return md4(p); },
+        hashcat: 900
+    },  {
+        name: 'SHA256($pass)',
+        func: function (p) { return sha256(p); },
+        hashcat: 1400
+    }, {
         name: 'SHA256($pass.$salt)',
-        func: function (n1, n2) { return sha256(n1 + n2); },
+        func: function (p, s) { return sha256(p + s); },
         hashcat: 1410
+    }, {
+        name: 'SHA256($salt.$pass)',
+        func: function (p, s) { return sha256(s + p); },
+        hashcat: 1420
+    }, {
+        name: 'MD5(MD5(MD5($pass)))',
+        func: function (p) { return md5(md5(md5(p))); },
+        hashcat: 3500
+    }, {
+        name: 'MD5(MD5($salt).$pass)',
+        func: function (p, s) { return md5(md5(s) + p); },
+        hashcat: 3610
+    }, {
+        name: 'MD5($salt.MD5($pass))',
+        func: function (p, s) { return md5(s + md5(p)); },
+        hashcat: 3710
+    }, {
+        name: 'MD5($pass.MD5($salt))',
+        func: function (p, s) { return md5(p + md5(s)); },
+        hashcat: 3720
+    }, {
+        name: 'MD5($salt.$pass.$salt)',
+        func: function (p, s) { return md5(s + p + s); },
+        hashcat: 3800
+    }, {
+        name: 'MD5(MD5($pass).MD5($salt))',
+        func: function (p, s) { return md5(md5(p) + md5(s)); },
+        hashcat: 3910
+    }, {
+        name: 'MD5($salt.MD5($salt.$pass))',
+        func: function (p, s) { return md5(s + md5(s + p)); },
+        hashcat: 4010
+    }, {
+        name: 'MD5($salt.MD5($pass.$salt))',
+        func: function (p, s) { return md5(s + md5(p + s)); },
+        hashcat: 4110
+    }, {
+        // TODO: Salt is not used
+        name: 'MD5($user.\'\\x00\'.MD5($salt))',
+        func: function (p, s, u) { return md5(u + '\x00' + p); },
+        hashcat: 4210
+    }, {
+        name: 'MD5(SHA1($pass))',
+        func: function (p) { return md5(sha1(p)); },
+        hashcat: 4400
+    }, {
+        name: 'SHA1(SHA1($pass))',
+        func: function (p) { return sha1(sha1(p)); },
+        hashcat: 4500
+    }, {
+        name: 'SHA1(SHA1(SHA1($pass)))',
+        func: function (p) { return sha1(sha1(sha1(p))); },
+        hashcat: 4600
+    }, {
+        name: 'SHA1(MD5($pass))',
+        func: function (p) { return sha1(md5(p)); },
+        hashcat: 4700
+    }, {
+        name: 'SHA1($salt.$pass.$salt)',
+        func: function (p, s) { return sha1(s + p + s); },
+        hashcat: 4900
     }];
 }]);
 
@@ -157,7 +217,7 @@ H.filter('filterMethods', function () {
             }
 
             // Filter by expected hash (TODO: Only works if hash.mode is hexadecimal)
-            var expectedHash = scope.hash.get();
+            var expectedHash = scope.hash.value;
             var resultingHash = method.func(tPass, tUser, tUser);
             return (resultingHash.indexOf(expectedHash) > -1);
         });
